23d17ba68c045d4b2499e51b193f4f709e9ae28a
Shivashish Jaiswal <shivashish987@gmail.com>
2020-01-30 15:17:24 +0530

    build commit

---
+++ build/precache-manifest.05769f000492d725d90a347c71878958.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dfac96d5318d6c60ede5998f64e0182",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd0de4fa9058cdc85d81",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2bfec2ad98e2760fb2e7",
+    "url": "/static/js/2.5292bfd3.chunk.js"
+  },
+  {
+    "revision": "fd0de4fa9058cdc85d81",
+    "url": "/static/js/main.e3a76d37.chunk.js"
+  },
+  {
+    "revision": "fbc2d0c349625ee50d4c",
+    "url": "/static/js/runtime-main.01f7e677.js"
+  }
+]);
